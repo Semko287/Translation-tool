437ce71c57967bd2f47098f61057a75b0f4933d5
Semko287 <semkogm@gmail.com>
2023-02-15 15:00:13 +0100

    project open

---
+++ js/game.js
@@ -0,0 +1,21 @@
+import Phaser from './phaser.min.js';
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
