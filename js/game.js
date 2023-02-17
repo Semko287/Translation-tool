c9ebbed66e02b82adee928362a1fca5a499057b1
Semko287 <semkogm@gmail.com>
2023-02-17 10:14:17 +0100

    basic structure

--- js/game.js
+++ js/game.js
@@ -1,21 +1,17 @@
-import Phaser from './phaser.min.js';
+import Phaser from './phaser.js';
 
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
+export default class Game extends Phaser.Scene 
+{
+    constructor() 
+    {
+        super('game');
+    }
+
+    preload() 
+    {
+    }
+
+    create() 
+    {
     }
 };
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-}
-
-function create ()
-{
-}
---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Phaser from "./phaser.js";
+import Game from "./game.js";
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 1920,
+	height: 1080,
+    backgroundColor: '#000000',
+    scene: Game
+});
